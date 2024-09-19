9d5e52d64a05cb5c36abae7d0a8a50c096ad5d9d
VictorGit <victornunes431@gmail.com>
2024-09-18 23:21:49 -0300

    Backend initialization

---
+++ backend/inovatec/src/main/java/com/br/inovatec/ProjetoInovatecApplication.java
@@ -0,0 +1,13 @@
+package com.br.inovatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoInovatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoInovatecApplication.class, args);
+	}
+
+}
