8d4c9f8fac27f9c5251a2c91967d22cf272183ca
VictorGit <victornunes431@gmail.com>
2024-09-21 17:46:14 -0300

    Test

--- backend/inovatec/src/main/java/com/br/inovatec/controller/UserController.java
+++ backend/inovatec/src/main/java/com/br/inovatec/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.br.inovatec.entity.User;
 import com.br.inovatec.service.UserService;
 
+//testtttttttttttttt
 @RestController
 @RequestMapping("/user")
 public class UserController {
