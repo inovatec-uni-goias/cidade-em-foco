3cf14c514588a6fc1f5a13197561b02b0f29f005
VictorGit <victornunes431@gmail.com>
2024-09-22 00:21:49 -0300

    Get and Set Id

--- backend/inovatec/src/main/java/com/br/inovatec/entity/User.java
+++ backend/inovatec/src/main/java/com/br/inovatec/entity/User.java
@@ -36,9 +36,18 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public User() {
+
     }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
 
     public String getLastName() {
         return lastName;
